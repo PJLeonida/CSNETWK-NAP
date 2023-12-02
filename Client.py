6aea031c3c38be5f480c9e5de7ae2a641c685062
PJLeonida <patrick_josh_c_leonida@dlsu.edu.ph>
2023-12-03 04:21:03 +0800

    Add register functionalities

--- Client.py
+++ Client.py
@@ -10,7 +10,7 @@ commanddesc = ["Connect to the server application", "Disconnect to the server ap
 def main():
     connected = False
     clientSocket = socket(AF_INET, SOCK_STREAM)
-
+    userconn = False
     while True:
         data = input("/ ")
         data = data.split(" ")
@@ -33,16 +33,37 @@ def main():
                 if not connected: # Displays error message if the Client is not connected to a Server
                     print("Error: Disconnection failed. Please connect to the server first.")
                 else:
+                    clientSocket.close()
                     break # Proceeds to Disconnect from the server
             elif command == "?": # Displays all commands
                 print("-COMMANDS-")
                 for cmd, desc in zip(commands, commanddesc):
                     print(cmd + " - " + desc)
                 print('\n')
+            elif command == "register":
+                if not connected: # Check if user is connected
+                    print("Error: Registration failed. Please connect to the server first.")
+                    continue
+                if len(data) != 2 or len(data) > 2: # Check if user inputted proper parameters
+                    raise SyntaxError("Invalid Parameters")
+                newUser = data[1] 
+                request = command + "@" + newUser
+
+                clientSocket.send(request.encode(FORMAT)) # Check if user handle is already used in server
+                response = clientSocket.recv(SIZE).decode(FORMAT)
+                response = response.split('@')
+
+                if response[1] == "Exists":  # User exists in server
+                    print("Error: Registration failed. Handle or alias already exists.")
+                    continue
+                
+                userconn = True
+                print("Hello, " + newUser + "!")
             else:
                 print("Error: Command not found.")
         except Exception:
             print("Error: Command parameters do not match or is not allowed.")
+
     print("Disconnected From Server...")
 
 if __name__ == "__main__":
--- Server.py
+++ Server.py
@@ -5,14 +5,36 @@ SIZE = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "Server Data"
 
-def clientHandler(connSocket, addr):
+def clientHandler(connSocket, addr, users):
     print(f"[CONNECTED] Connection with {addr} established.")
     connSocket.send("OK$Welcome to the File Exchange Server".encode(FORMAT))
+
+    while True: 
+        data = connSocket.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        command = data[0]
+
+        if command == "register":
+            response = "OK@"
+            newUser = data[1] # Expects command to be /register <Alias>
+
+            if newUser in users: # If user alias exists, send 'Exists', Otherwise send DNE
+                response += "Exists"
+                connSocket.send(response.encode(FORMAT))
+                continue
+
+            users.append(newUser) # Append user to list of registered users
+            response += "DNE"
+            connSocket.send(response.encode(FORMAT))
+
+            
+
 
 def main():
     serverIP = 'localhost'
     serverPort = 6969
     bind = (serverIP, serverPort)
+    users = []
 
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(bind)
@@ -20,7 +42,7 @@ def main():
     print("Server is listening...")
     while True:
         connSocket, addr = serverSocket.accept()
-        thread = threading.Thread(target=clientHandler, args = (connSocket, addr))
+        thread = threading.Thread(target=clientHandler, args = (connSocket, addr, users))
         thread.start()
 
 if __name__ == "__main__":
